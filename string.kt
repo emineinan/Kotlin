2772fa171f4b80d4bfff77bece29d574fa1b419f
emineinan <56589369+emineinan@users.noreply.github.com>
2020-10-02 17:43:10 +0300

    string is added.

---
+++ string.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val myString="Hello Kotlin"
+    println(myString.length)
+    
+    val name="Alice"
+    val age="24"
+    val space=" "
+    val x=name+space+age
+    println(x)
+    
+    val newString:String
+    newString="10"
+    println(newString)
+}
