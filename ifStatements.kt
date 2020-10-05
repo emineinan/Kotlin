bbd40bec91fe4c15830bc4bebca2ba658b9926c3
emineinan <56589369+emineinan@users.noreply.github.com>
2020-10-05 12:10:21 +0300

    if statements is added.

---
+++ ifStatements.kt
@@ -0,0 +1,10 @@
+fun main() {
+   val grade=80 
+   if(grade<50){
+       println("You are failed from exam.")
+   }else if(grade>=50 && grade<=70){
+       println("Your grade is good.")
+   }else{
+       println("You are very succcesssful")
+   }
+}
