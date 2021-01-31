051f7c57543fadeb4bea3e0bdc1c64bcd38f1c39
emineinan <56589369+emineinan@users.noreply.github.com>
2021-01-31 17:03:39 +0300

    Add sample to static polymorphism

---
+++ static_polymorphism.kt
@@ -0,0 +1,14 @@
+class Calculation(){
+   fun multiply(a:Int, b:Int) : Int {
+       return a*b
+   }
+   fun multiply(a:Int, b:Int, c:Int) : Int {
+       return a*b*c
+   }
+}
+fun main() {
+   //Static Polymorphism
+   val calculation=Calculation() 
+   println(calculation.multiply(2,5))
+   println(calculation.multiply(2,5,3))
+}
